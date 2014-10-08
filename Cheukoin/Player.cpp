e62fcb2b4d84459a1c43288f33dfffc881d3a13e
Gabriel Samain <gabriel.samain@gmail.com>
2014-10-08 11:54:53 +0200

    getter/setter Player

--- Cheukoin/Player.cpp
+++ Cheukoin/Player.cpp
@@ -11,3 +11,33 @@ Player::~Player()
 }
 
 void Player::playCard(Card card) {}
+
+string Player::getName()
+{
+    return _name;
+}
+
+void Player::setName(string name)
+{
+    _name = name;
+}
+
+Hand Player::getHand()
+{
+    return _hand;
+}
+
+void Player::setHand(Hand hand)
+{
+    _hand = hand;
+}
+
+Bid Player::getBid()
+{
+    return _bid;
+}
+
+void Player::setBid(Bid bid)
+{
+    _bid = bid;
+}
