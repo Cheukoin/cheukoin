d16ced0d34a603c5f08c53fe143c45c1ac1dcebd
Adrien Bruchet <adrien.bruchet.gt@gmail.com>
2014-10-08 12:12:40 +0200

    rules wip

--- Rules.cpp
+++ Rules.cpp
@@ -7,8 +7,11 @@
 //
 
 #include "Rules.h"
+#include <map>
 
-Rules::Rules():_cardValuesAsset({{Seven, 0}, {Eight, 0}, {Nine, 14}, {Ten, 10}, {Jack, 20}, {Queen, 3}, {King, 4}, {Ace, 11}}), _cardValues({{Seven,0},{Eight,0},{Nine,0},{Ten,10},{Jack,2},{Queen,3},{King,4},{Ace,11}})
+using namespace std;
+
+Rules::Rules():_cardValuesAsset({{Seven, 0}, {Eight, 0}, {Nine, 14}, {Ten, 10}, {Jack, 20}, {Queen, 3}, {King, 4}, {Ace, 11}}), _cardValues({{Seven,0}, {Eight,0}, {Nine,0}, {Ten,10}, {Jack,2}, {Queen,3}, {King,4}, {Ace,11}})
 {
     
 }
@@ -20,7 +23,41 @@ Rules::~Rules()
 
 bool Rules::isGreater(Card card1, Card card2, Suit asset)
 {
-    
+    if (card1.getSuit() != asset && card2.getSuit() == asset)
+    {
+        return false;
+    }
+    else if (card1.getSuit() == asset && card2.getSuit() != asset)
+    {
+        return true;
+    }
+    else
+    {
+        map <Value, int> order;
+        
+        if (card1.getSuit() != card2.getSuit())
+        {
+            return false;
+        }
+        else if (card1.getSuit() == asset)
+        {
+            order = _cardValuesAsset;
+        }
+        else
+        {
+            order = _cardValues;
+        }
+        
+        if (order[card1.getValue()] > order[card2.getValue()])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
 
 Card Rules::winningCard(Trick trick, Suit asset)
