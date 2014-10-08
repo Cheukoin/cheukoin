40ea613a0d92411f68187d70d47e468542d2e51f
Gabriel Samain <gabriel.samain@gmail.com>
2014-10-08 11:24:51 +0200

    add getter setter player

--- Cheukoin/Player.h
+++ Cheukoin/Player.h
@@ -6,10 +6,16 @@ class Player {
 public:
     Player();
     ~Player();
+
     void playCard(Card card);
     Bid makeBid(int amount, std::string asset);
+
     std::string getName();
     void setName(std::string name);
+    Hand getHand();
+    void setHand(Hand hand);
+    Bid getBid();
+    void setBid(Bid bid);
 
 private:
     std::string _name;
