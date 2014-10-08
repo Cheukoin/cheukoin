9fda747fc0b1c4a7f3931a227429ba744c98d25e
Adrien Bruchet <adrien.bruchet.gt@gmail.com>
2014-10-08 12:02:21 +0200

    rules skeleton

--- Rules.cpp
+++ Rules.cpp
@@ -7,3 +7,24 @@
 //
 
 #include "Rules.h"
+
+Rules::Rules():_cardValuesAsset({{Seven, 0}, {Eight, 0}, {Nine, 14}, {Ten, 10}, {Jack, 20}, {Queen, 3}, {King, 4}, {Ace, 11}}), _cardValues({{Seven,0},{Eight,0},{Nine,0},{Ten,10},{Jack,2},{Queen,3},{King,4},{Ace,11}})
+{
+    
+}
+
+Rules::~Rules()
+{
+    
+}
+
+bool Rules::isGreater(Card card1, Card card2, Suit asset)
+{
+    
+}
+
+Card Rules::winningCard(Trick trick, Suit asset)
+{
+    
+}
+
