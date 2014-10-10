ede3e9d3c2c562de50fef86258d71af25c463fd2
Adrien Bruchet <adrien.bruchet.gt@gmail.com>
2014-10-10 09:54:08 +0200

    rules wip

--- Rules.h
+++ Rules.h
@@ -25,5 +25,5 @@ private:
     std::map<Value, int> _cardValues;
     std::map<Value, int> _cardValuesAsset;
 };
-
+/ Users / adrienbruchet / Library / Containers / com.apple.mail / Data / Library / Mail Downloads / 7B3B11EC - 0751 - 4E91 - 876C - 98F7E01F293A / CodeFile1.c
 #endif /* defined(__Cheukoin__Rules__) */
