b81a7f640b47d97601d6002358053a754247cb22
Salma-Sl <slimi1salma@gmail.com>
2014-10-30 20:46:14 +0100

    Text pos

--- Cheukoin/Score.cpp
+++ Cheukoin/Score.cpp
@@ -29,15 +29,15 @@ void Score::displayScore(int score1, int score2, std::shared_ptr<sf::RenderWindo
     if (!font.loadFromFile(resourcePath("sansation.ttf")))
         puts("_texture file not loaded");
 
-    _text1->setPosition(100, 60);
+    _text1->setPosition(window->getSize().x * 0.1, window->getSize().y * 0.1);
     _text1->setFont(font);
     _text1->setString("Team 1 : " + to_string(score1));
-    _text1->setCharacterSize(30);
+    _text1->setCharacterSize(window->getSize().y * 0.05);
     _text1->setColor(sf::Color::Red);
-    _text2->setPosition(100, 100);
+    _text2->setPosition(window->getSize().x * 0.1, window->getSize().y * 0.15);
     _text2->setFont(font);
     _text2->setString("Team 2 : " + to_string(score2));
-    _text2->setCharacterSize(30);
+    _text2->setCharacterSize(window->getSize().y * 0.05);
     _text2->setColor(sf::Color::Red);
     window->draw(*_text1);
     window->draw(*_text2);
